db0aacaa2a62f43b04146c2dcf55af55a27b2622
HarryPotter0308 <dmitrydenisov56@gmail.com>
2021-12-16 00:05:00 +1000

    Update

--- helper.js
+++ helper.js
@@ -1,78 +1,230 @@
-Contribution: 2021-11-07 00:00
+Contribution: 2020-08-01 00:00
 
-Contribution: 2021-11-07 00:01
+Contribution: 2020-08-01 00:01
 
-Contribution: 2021-11-07 00:02
+Contribution: 2020-08-01 00:02
 
-Contribution: 2021-11-27 00:00
+Contribution: 2020-08-01 00:03
 
-Contribution: 2021-11-27 00:01
+Contribution: 2020-08-01 00:04
 
-Contribution: 2021-11-27 00:02
+Contribution: 2020-08-02 00:00
 
-Contribution: 2021-11-27 00:03
+Contribution: 2020-08-02 00:01
 
-Contribution: 2021-11-27 00:04
+Contribution: 2020-08-02 00:02
 
-Contribution: 2021-11-27 00:05
+Contribution: 2020-08-03 00:00
 
-Contribution: 2021-11-27 00:06
+Contribution: 2020-08-09 00:00
 
-Contribution: 2021-11-27 00:07
+Contribution: 2020-08-09 00:01
 
-Contribution: 2021-11-27 00:08
+Contribution: 2020-08-09 00:02
 
-Contribution: 2021-11-27 00:09
+Contribution: 2020-08-09 00:03
 
-Contribution: 2021-11-28 00:00
+Contribution: 2020-08-09 00:04
 
-Contribution: 2021-11-28 00:01
+Contribution: 2020-08-09 00:05
 
-Contribution: 2021-11-28 00:02
+Contribution: 2020-08-09 00:06
 
-Contribution: 2021-11-28 00:03
+Contribution: 2020-08-09 00:07
 
-Contribution: 2021-11-28 00:04
+Contribution: 2020-08-09 00:08
 
-Contribution: 2021-12-09 00:00
+Contribution: 2020-08-10 00:00
 
-Contribution: 2021-12-10 00:00
+Contribution: 2020-08-10 00:01
 
-Contribution: 2021-12-12 00:00
+Contribution: 2020-08-10 00:02
 
-Contribution: 2021-12-12 00:01
+Contribution: 2020-08-10 00:03
 
-Contribution: 2021-12-12 00:02
+Contribution: 2020-08-10 00:04
 
-Contribution: 2021-12-12 00:03
+Contribution: 2020-08-10 00:05
 
-Contribution: 2021-12-12 00:04
+Contribution: 2020-08-10 00:06
 
-Contribution: 2021-12-12 00:05
+Contribution: 2020-08-12 00:00
 
-Contribution: 2021-12-12 00:06
+Contribution: 2020-08-12 00:01
 
-Contribution: 2021-12-12 00:07
+Contribution: 2020-08-12 00:02
 
-Contribution: 2021-12-12 00:08
+Contribution: 2020-08-12 00:03
 
-Contribution: 2021-12-12 00:09
+Contribution: 2020-08-16 00:00
 
-Contribution: 2021-12-13 00:00
+Contribution: 2020-08-16 00:01
 
-Contribution: 2021-12-13 00:01
+Contribution: 2020-08-16 00:02
 
-Contribution: 2021-12-13 00:02
+Contribution: 2020-08-16 00:03
 
-Contribution: 2021-12-13 00:03
+Contribution: 2020-08-18 00:00
 
-Contribution: 2021-12-16 00:00
+Contribution: 2020-08-18 00:01
 
-Contribution: 2021-12-16 00:01
+Contribution: 2020-08-19 00:00
 
-Contribution: 2021-12-16 00:02
+Contribution: 2020-08-19 00:01
 
-Contribution: 2021-12-16 00:03
+Contribution: 2020-08-20 00:00
 
-Contribution: 2021-12-16 00:04
+Contribution: 2020-08-20 00:01
+
+Contribution: 2020-08-20 00:02
+
+Contribution: 2020-08-20 00:03
+
+Contribution: 2020-08-20 00:04
+
+Contribution: 2020-08-20 00:05
+
+Contribution: 2020-08-23 00:00
+
+Contribution: 2020-08-23 00:01
+
+Contribution: 2020-08-23 00:02
+
+Contribution: 2020-08-23 00:03
+
+Contribution: 2020-08-23 00:04
+
+Contribution: 2020-08-27 00:00
+
+Contribution: 2020-08-27 00:01
+
+Contribution: 2020-08-27 00:02
+
+Contribution: 2020-08-27 00:03
+
+Contribution: 2020-08-27 00:04
+
+Contribution: 2020-08-27 00:05
+
+Contribution: 2020-08-28 00:00
+
+Contribution: 2020-08-28 00:01
+
+Contribution: 2020-08-28 00:02
+
+Contribution: 2020-08-28 00:03
+
+Contribution: 2020-08-28 00:04
+
+Contribution: 2020-08-28 00:05
+
+Contribution: 2020-08-28 00:06
+
+Contribution: 2020-08-31 00:00
+
+Contribution: 2020-09-02 00:00
+
+Contribution: 2020-09-02 00:01
+
+Contribution: 2020-09-04 00:00
+
+Contribution: 2020-09-04 00:01
+
+Contribution: 2020-09-04 00:02
+
+Contribution: 2020-09-04 00:03
+
+Contribution: 2020-09-04 00:04
+
+Contribution: 2020-09-04 00:05
+
+Contribution: 2020-09-04 00:06
+
+Contribution: 2020-09-04 00:07
+
+Contribution: 2020-09-04 00:08
+
+Contribution: 2020-09-04 00:09
+
+Contribution: 2020-09-05 00:00
+
+Contribution: 2020-09-05 00:01
+
+Contribution: 2020-09-05 00:02
+
+Contribution: 2020-09-05 00:03
+
+Contribution: 2020-09-05 00:04
+
+Contribution: 2020-09-05 00:05
+
+Contribution: 2020-09-06 00:00
+
+Contribution: 2020-09-06 00:01
+
+Contribution: 2020-09-06 00:02
+
+Contribution: 2020-09-06 00:03
+
+Contribution: 2020-09-06 00:04
+
+Contribution: 2020-09-06 00:05
+
+Contribution: 2020-09-06 00:06
+
+Contribution: 2020-09-06 00:07
+
+Contribution: 2020-09-06 00:08
+
+Contribution: 2020-09-09 00:00
+
+Contribution: 2020-09-09 00:01
+
+Contribution: 2020-09-09 00:02
+
+Contribution: 2020-09-10 00:00
+
+Contribution: 2020-09-10 00:01
+
+Contribution: 2020-09-10 00:02
+
+Contribution: 2020-09-10 00:03
+
+Contribution: 2020-09-11 00:00
+
+Contribution: 2020-09-11 00:01
+
+Contribution: 2020-09-12 00:00
+
+Contribution: 2020-09-12 00:01
+
+Contribution: 2020-09-13 00:00
+
+Contribution: 2020-09-13 00:01
+
+Contribution: 2020-09-13 00:02
+
+Contribution: 2020-09-13 00:03
+
+Contribution: 2020-09-15 00:00
+
+Contribution: 2020-09-15 00:01
+
+Contribution: 2020-09-18 00:00
+
+Contribution: 2020-09-18 00:01
+
+Contribution: 2020-09-18 00:02
+
+Contribution: 2020-09-18 00:03
+
+Contribution: 2020-09-19 00:00
+
+Contribution: 2020-09-19 00:01
+
+Contribution: 2020-09-21 00:00
+
+Contribution: 2020-09-21 00:01
+
+Contribution: 2021-12-16 00:05
 
